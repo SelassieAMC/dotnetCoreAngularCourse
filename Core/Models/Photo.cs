470453e4216663a449bed20b701f664341db8fd5
Andres Mauricio Clavijo Mejia <aclavime@emtelco.com.co>
2019-09-03 17:05:33 -0500

    Extending Domain Model

---
+++ Core/Models/Photo.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Vegas.Core.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string FileName { get; set; }
+    }
+}
